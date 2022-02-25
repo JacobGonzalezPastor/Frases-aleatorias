9315311cb8e7fea96f02e33fecd535f2297b9614
Jacob Gonzalez <jacobenrrique@hotmial.com>
2022-02-24 22:26:22 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import { Button, Icon } from 'semantic-ui-react';
 function App() {
+
+  const [fraseInfo, setFraseInfo] = useState({})
+
+  useEffect(() => {
+    getFrase();
+  }, []);
+
+  const getFrase = () => {
+    fetch('https://api.quotable.io/random')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFraseInfo({
+          texto: data.content,
+          autor: data.author
+        })
+        console.log(data);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='caja_frases'>
+        <p id='texto'>{fraseInfo.texto}</p>
+        <p id='autor'>{fraseInfo.autor}</p>
+        <Button id='nueva-frase' onClick={getFrase} color='youtube'>
+          <Icon name='random' /> Nueva Frase</Button>
+        <br />
+        <Button id='tweet-frase' href='#' color='twitter'>
+          <Icon name='twitter' /> Postear en twitter</Button>
+      </div>
     </div>
   );
 }
